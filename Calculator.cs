ec222357198246428f78efa0bd2c0e30450ee247
Zack Way <zack.way@kizan.com>
2021-06-06 17:13:27 -0400

    Moving calculator to it's own file

---
+++ Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace GitTraining
+{
+    internal class Calculator
+    {
+        public static async Task<string> Parse(string input)
+        {
+            return (await CSharpScript.EvaluateAsync(input)).ToString();
+        }
+    }
+}
