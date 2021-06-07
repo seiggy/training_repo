66a5686204e5a6c64d2c8f471e6a2d5a26d587b2
Richard Kim <richard.kim@emerson.com>
2021-06-07 14:26:51 -0500

    Moved Calculator class to separate file.

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTraining
+{
+    internal class Calculator
+    {
+        public static async Task<string> Parse(string input)
+        {
+            return (await CSharpScript.EvaluateAsync(input)).ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,12 +15,4 @@ namespace GitTraining
             Console.ReadKey();
         }
     }
-
-    internal class Calculator
-    {
-        public static async Task<string> Parse(string input)
-        {
-            return (await CSharpScript.EvaluateAsync(input)).ToString();
-        }
-    }
 }
