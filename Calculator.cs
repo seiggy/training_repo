f57e916f74ccc9bab8a4a79eeca9ae1ebac3a1f5
Amanda McConville <amanda.mcconville@emerson.com>
2021-06-07 14:38:56 -0500

    message

--- Calculator.cs
+++ Calculator.cs
@@ -3,5 +3,6 @@
         public static async Task<string> Parse(string input)
         {
             return (await CSharpScript.EvaluateAsync(input)).ToString();
+            #something
         }
     }
