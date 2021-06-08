0f37ec9fb38c9430b25ed82e8aa0469225b5a684
Zack Way <zack.way@kizan.com>
2021-06-08 14:41:19 -0400

    Moved calculator class to it's own file

---
+++ Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace GitTraining
+{
+    internal class Calculator
+    {
+        public static async Task<string> Parse(string input)
+        {
+            return (await CSharpScript.EvaluateAsync(input)).ToString();
+        }
+    }
+}
